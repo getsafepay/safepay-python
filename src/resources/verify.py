a289acd63e79a8e32ae5ec8d247fa00a07fccf82
mrsaeeddev <saeed_dev@yahoo.com>
2023-01-29 08:40:29 +0500

    add class for verify - sig and webhook

---
+++ src/resources/verify.py
@@ -0,0 +1,24 @@
+import Crypto.Hash import SHA256, SHA512
+
+class Verify:
+  def __init__(self, config):
+    self.config = config
+
+  def signature(self, request):
+    sig = request.body['sig']
+    tracker = request.body['tracker']
+    hash = SHA256.new(self.config.v1Secret)
+    hash.update(tracker)
+    hash.digest('hex')
+
+    return sig == hash
+
+  def webhook(self, request):
+    data = memoryview(json.dumps(request.body['data'], separators=(',', ':')))
+    signature = ['x-sfpy-signature']
+
+    hash = SHA512.new(self.config.webhookSecret)
+    hash.update(data)
+    hash.digest('hex')
+
+    return signature = hash
