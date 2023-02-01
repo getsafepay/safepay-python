2e2e9ce94c5788268ef1c8f515c88e7bc0302452
fatimaaurangzeb0640 <fatimaaurangzeb0640@gmail.com>
2023-02-01 16:02:31 +0500

    removed commented code

--- src/resources/verify.py
+++ src/resources/verify.py
@@ -1,5 +1,3 @@
-from Crypto.Hash import SHA256, SHA512
-from Crypto.Hash import HMAC, SHA256
 import json
 from hashlib import sha256, sha512
 import hmac
@@ -34,29 +32,6 @@ class Verify:
 
         return sig == digestedMac
 
-        # comptuted_sig = mac
-
-        # print(comptuted_sig)
-
-        # print(comptuted_sig == h.hexdigest())
-
-        # h = HMAC.new(bytes(secret, encoding='utf-8'), digestmod=SHA256)
-        # print(h)
-        # h.update(bytes(tracker, encoding='utf-8'))
-        # print(h.hexdigest())
-
-        # secret = b'Swordfish'
-        # h = HMAC.new(secret, digestmod=SHA256)
-        # h.update(b'Hello')
-        # print(h.hexdigest())
-
-        # hash = SHA256.new(self.config['v1Secret'])
-        # hash.update(tracker)
-        # hash.digest('hex')
-
-        #
-        # return secret == sig
-
     def webhook(self, header, body):
         signature = (header['x-sfpy-signature'])
         print(f'signature: {signature}')
@@ -75,32 +50,3 @@ class Verify:
         comparison_res = secure_compare(comptuted_sig, signature)
         print(f'comparison res: {comparison_res}')
         return comparison_res
-
-# Fatima's
-
-# signature = (header['x-sfpy-signature'])
-# print(f'sig: {signature}')
-# stringifiedData = json.dumps(
-#     body['data'], separators=(',', ':'))
-# print(f'data: {stringifiedData}')
-# a = bytearray(stringifiedData, 'utf-8')
-# m = memoryview(a)
-# print(m)
-# secret = self.config['v1Secret']
-# h = HMAC.new(bytes(secret, encoding='utf-8'), digestmod=SHA512)
-# print(h)
-# h.update(m)
-# print(h.hexdigest())
-# return signature == h
-
-# Saeed's
-
-# data = bytes(memoryview(json.dumps(
-#     body['data'], separators=(',', ':'))).encode())
-# print(data)
-# signature = ['x-sfpy-signature']
-
-# hash = SHA512.new(self.config.webhookSecret)
-# hash.update(data)
-# hash.digest('hex')
-# return signature == h
