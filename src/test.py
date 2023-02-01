46ceae1e400c038af4f0c8c2758d4870a785cea6
fatimaaurangzeb0640 <fatimaaurangzeb0640@gmail.com>
2023-02-01 16:40:21 +0500

    hard-coded variables and stored in constants.py

--- src/test.py
+++ src/test.py
@@ -1,19 +1,14 @@
 from index import *
 import asyncio
-import requests
+from utils.constants import *
 
 # Object creation test
 
-# ENVIRONMENT = 'sandbox'
-# TEST_API_KEY = ""
-# TEST_V1_SECRET_KEY =
-# TEST_WEBHOOK_SECRET_KEY =
-
 env = Safepay({
-    'environment': 'sandbox',
-    'apiKey': 'sec_9286c6a3-a159-492d-9f72-dbe424517fb5',
-    'v1Secret': 'd928d9b1d9e487957c6658be637621bbb104d64b6b436e86714c2ff3712bd825',
-    'webhookSecret': 'be0001c975bbfd4f78fa3af9e8a3aeae14aab420c859f2255e12f532028a38f5'
+    'environment': ENVIRONMENT,
+    'apiKey': TEST_API_KEY,
+    'v1Secret': TEST_V1_SECRET_KEY,
+    'webhookSecret': TEST_WEBHOOK_SECRET_KEY
 })
 
 print(f'environment: {env.config}')
@@ -26,13 +21,13 @@ payment_response = asyncio.run(env.set_payment_details({'amount': 10000,
 
 print(f'paymentResponse: {payment_response}')
 
-token = (payment_response['data'])['token']
+# token = (payment_response['data'])['token']
 
-print(f'token: {token}')
+# print(f'token: {token}')
 
 # Checkout url test
 
-checkout_url = env.get_checkout_url({'beacon': token,
+checkout_url = env.get_checkout_url({'beacon': TOKEN,
                                      'cancelUrl': 'http://example.com/cancel',
                                      'orderId': 'T800',
                                      'redirectUrl': 'http://example.com/success',
@@ -43,43 +38,24 @@ print(f'checkoutURL: {checkout_url}')
 
 # Verification test for signatures
 
-mac = hmac.new('d928d9b1d9e487957c6658be637621bbb104d64b6b436e86714c2ff3712bd825'.encode('utf-8'),
-               msg=token.encode('utf-8'),
-               digestmod=sha256)
+# mac = hmac.new('d928d9b1d9e487957c6658be637621bbb104d64b6b436e86714c2ff3712bd825'.encode('utf-8'),
+#                msg=TOKEN.encode('utf-8'),
+#                digestmod=sha256)
 
-sig = mac.hexdigest()
+# sig = mac.hexdigest()
 
-print(sig)
+# print(sig)
 
-verification_response = env.is_signature_valid({'sig': sig,
-                                                'tracker': token})
+verification_response = env.is_signature_valid({'sig': TEST_SIGNATURE,
+                                                'tracker': TOKEN})
 
 print(f'signature verification response: {verification_response}')
 
 # Verification test for webhooks
 
-wh_verification_response = env.is_webhook_valid({'x-sfpy-signature': '910870282c2b040626293b8d7d2b3046cd14fc00225febf42efc05cf160eb391aca739470e368a0670dfab3877f4e7eda0da2ac760ca50017ac1b2ef971ada31'
+wh_verification_response = env.is_webhook_valid({'x-sfpy-signature': TEST_WEBHOOK_SIGNATURE
                                                  },
-                                                {'data': {'client_id': 'sec_9286c6a3-a159-492d-9f72-dbe424517fb5',
-                                                          'created_at': '2021-09-29T12:00:40Z',
-                                                          'endpoint': 'http://127.0.0.1:9000',
-                                                          'notification': {
-                                                              'amount': '150',
-                                                              'currency': 'PKR',
-                                                              'fee': '4.92',
-                                                              'intent': 'PAYFAST',
-                                                              'metadata': {
-                                                                  'order_id': 'XG102312',
-                                                                  'source': 'shopify'
-                                                              },
-                                                              'net': '145.08',
-                                                              'state': 'PAID',
-                                                              'tracker': 'tracker_c5a5apsbcv41om3fg0u0',
-                                                              'user': 'johndoe@gmail.com'
-                                                          },
-                                                          'token': 'C5A5APSBCV41R2QF2MHG',
-                                                          'type': 'payment:created',
-                                                          'updated_at': '2021-09-29T12:00:40Z'}})
+                                                {'data': TEST_WEBHOOK_DATA})
 
 print(f'webhook verification response: {wh_verification_response}')
 
--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,5 +1,33 @@
 from enum import Enum
 
+ENVIRONMENT = 'sandbox'
+TEST_API_KEY = 'sec_9286c6a3-a159-492d-9f72-dbe424517fb5'
+TEST_V1_SECRET_KEY = 'd928d9b1d9e487957c6658be637621bbb104d64b6b436e86714c2ff3712bd825'
+TEST_WEBHOOK_SECRET_KEY = 'be0001c975bbfd4f78fa3af9e8a3aeae14aab420c859f2255e12f532028a38f5'
+TOKEN = 'track_9511f95d-2c77-4fc7-9474-0dcca462f09a'
+TEST_SIGNATURE = '50f339f5d9a28ece5f72a5dc208972fe468d3898427a54b6ed7df9be7c406b09'
+TEST_WEBHOOK_SIGNATURE = '910870282c2b040626293b8d7d2b3046cd14fc00225febf42efc05cf160eb391aca739470e368a0670dfab3877f4e7eda0da2ac760ca50017ac1b2ef971ada31'
+TEST_WEBHOOK_DATA = {'client_id': 'sec_9286c6a3-a159-492d-9f72-dbe424517fb5',
+                     'created_at': '2021-09-29T12:00:40Z',
+                     'endpoint': 'http://127.0.0.1:9000',
+                     'notification': {
+                         'amount': '150',
+                         'currency': 'PKR',
+                         'fee': '4.92',
+                         'intent': 'PAYFAST',
+                         'metadata': {
+                             'order_id': 'XG102312',
+                             'source': 'shopify'
+                         },
+                         'net': '145.08',
+                         'state': 'PAID',
+                         'tracker': 'tracker_c5a5apsbcv41om3fg0u0',
+                         'user': 'johndoe@gmail.com'
+                     },
+                     'token': 'C5A5APSBCV41R2QF2MHG',
+                     'type': 'payment:created',
+                     'updated_at': '2021-09-29T12:00:40Z'}
+
 API_URL_PRODUCTION = 'https://api.getsafepay.com'
 API_URL_SANDBOX = 'https://sandbox.api.getsafepay.com'
 API_URL_DEVELOPMENT = 'https://dev.api.getsafepay.com'
@@ -8,8 +36,8 @@ CHECKOUT_PRODUCTION = 'https://getsafepay.com/checkout/pay'
 CHECKOUT_SANDBOX = 'https://sandbox.api.getsafepay.com/checkout/pay'
 CHECKOUT_DEVELOPMENT = 'https://dev.api.getsafepay.com/checkout/pay'
 
-class Environment(Enum):
-  Production = 'production'
-  Sandbox = 'sandbox'
-  Development = 'development'
 
+class Environment(Enum):
+    Production = 'production'
+    Sandbox = 'sandbox'
+    Development = 'development'
